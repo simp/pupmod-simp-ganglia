cff4fa9d368b0419a43ba8ccebe4b44069f6c587
nmarkowski <nmarkowski@keywcorp.com>
2016-10-19 20:03:00 -0400

    (SIMP-1723) Fixed invalid simpcat dependency and updated spec
    - Converted apache::add_site to simp_apache::add_site
    - Included a required simpcat_build(gweb), for the $auth_user_file
    
    SIMP-1723 #close
    
    Change-Id: Ifb489ad4f6137fb59677aaa0bfbb1bfdb2eb0d34

--- spec/classes/web/conf_spec.rb
+++ spec/classes/web/conf_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe 'ganglia' do
+describe 'ganglia::web::conf' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
         let(:facts) { facts }
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_class('ganglia') }
-        it { is_expected.to create_file('/etc/ganglia').with_ensure('directory') }
+        it { is_expected.to contain_simpcat_build('gweb') }
+        it { is_expected.to contain_file('/usr/local/apache/ganglia-passwords').that_requires('Simpcat_build[gweb]') }
       end
     end
   end
